869f6371e57db6f52628e9926604317552a568a5
Dmitry Dmitruk <dmitry.dmitruk1985@gmail.com>
2021-03-15 16:49:11 +0300

    Подключение iOS к тестам и первый тест

---
+++ src/lib/iOSTestCase.java
@@ -0,0 +1,47 @@
+package lib;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
+import junit.framework.TestCase;
+import org.openqa.selenium.ScreenOrientation;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+
+public class iOSTestCase extends TestCase {
+    protected AppiumDriver driver;
+    private static String AppiumURL = "http://127.0.0.1:4723/wd/hub";
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName", "iOS");
+        capabilities.setCapability("deviceName", "iPhone SE");
+        capabilities.setCapability("platformVersion", "11.3");
+        capabilities.setCapability("automationName", "Appium");
+        capabilities.setCapability("app", "D:\\Documents\\IT\\Testing\\Mobile_Automation\\github\\JavaAppiumAutomation\\apks\\Wikipedia.app");
+        capabilities.setCapability("orientation", "PORTRAIT");
+
+        driver = new IOSDriver(new URL(AppiumURL), capabilities);
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        driver.quit();
+        super.tearDown();
+    }
+
+    protected void rotateScreenPortrait(){
+        driver.rotate(ScreenOrientation.PORTRAIT);
+    }
+
+    protected void rotateScreenLandscape(){
+        driver.rotate(ScreenOrientation.LANDSCAPE);
+    }
+
+    protected void backgroundApp(int seconds){
+        driver.runAppInBackground(seconds);
+    }
+}
